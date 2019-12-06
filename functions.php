0f2914423bb92b65a399df6e5a3507dbe4a0353b
Hazuka <hazukashii.nee@gmail.com>
2019-12-06 11:55:59 +0700

    Add content template part, add manga's info field

--- functions.php
+++ functions.php
@@ -11,3 +11,16 @@ if (!function_exists('mimi_theme_setup')) {
   }
   add_action('after_theme_setup', 'mimi_theme_setup');
 }
+
+function mimi_title_bar($title, $permalink) { ?>
+  <div class="title-bar">
+    <div class="row">
+      <div class="col">
+        <h2 class="title"><?php echo $title; ?></h2>
+      </div>
+      <div class="col text-right">
+        <a class="btn-view" href="<?php echo $permalink; ?>">See All&nbsp;&nbsp;<i class="fas fa-caret-right"></i></a>
+      </div>
+    </div>
+  </div>
+<?php }
---
+++ template-parts/carousel.php
@@ -0,0 +1,15 @@
+<div class="posts-area">
+  <div class="owl-carousel">
+    <?php for ($i = 0; $i < 4; ++$i): ?>
+      <div class="item">
+        <div class="wrap-post">
+          <a href="#">
+            <img class="owl-lazy" data-src="https://m.media-amazon.com/images/M/MV5BMWRlMDU2NWMtOTAyNC00OTJhLTg3OGQtMmQ1YjQ0M2IyMTNiXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg" alt="">
+            <h3 class="title">Suzuka Ohgo</h3>
+          </a>
+          <span class="views"><i class="fas fa-eye"></i> 6969</span>
+        </div>
+      </div>
+    <?php endfor; ?>
+  </div>
+</div>
