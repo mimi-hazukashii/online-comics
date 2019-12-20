b126e720114ec76911043ae1f66e2eb157990c34
Mimi <hazukashii.nee@gmail.com>
2019-12-20 14:58:34 +0700

    Add sort for found posts

--- search.php
+++ search.php
@@ -1,27 +1,54 @@
 <?php
 get_header();
 
+$order = 'DESC';
+$order_by = 'date';
+
+if (isset($_GET['sort'])) {
+  switch ($_GET['sort']) {
+    case 'views':
+      $order = 'DESC';
+      $order_by = 'meta_value_num';
+      break;
+    case 'asc':
+      $order = 'ASC';
+      $order_by = 'title';
+      break;
+    case 'desc':
+      $order = 'DESC';
+      $order_by = 'title';
+      break;
+    default:
+      $order = 'DESC';
+      $order_by = 'date';
+  }
+}
+
 $args = array(
+  'post_type' => 'post',
   'posts_per_page' => get_option('posts_per_page'),
   'paged' => get_query_var('paged') ? get_query_var('paged') : 1,
+  'order' => $order,
+	'orderby' => $order_by,
   's' => isset($_GET['s']) ? $_GET['s'] : null
-);
-
-$menu = array(
-	array(
-		'name' => 'Views',
-		'url' => '/all?sort=views'
-	), array(
-		'name' => 'Title ASC',
-		'url' => '/all?sort=asc'
-	), array(
-		'name' => 'Title DESC',
-		'url' => '/all?sort=desc'
-	)
 );
 
 $posts = new WP_Query($args);
 
+$s = str_replace(' ', '+', $posts->query_vars['s']);
+
+$menu = array(
+  array(
+    'name' => 'Views',
+    'url' => "/?s=$s&sort=views"
+  ), array(
+    'name' => 'Title ASC',
+    'url' => "/?s=$s&sort=asc"
+  ), array(
+    'name' => 'Title DESC',
+    'url' => "/?s=$s&sort=desc"
+  )
+);
 if ($posts->have_posts()): ?>
   <main>
     <article>
